32a07837f0730fd3477450c91ceb3895a15e336c
Sandeep Kumar <sandeepkhoj@gmail.com>
2023-11-20 22:22:26 +0530

    Updates

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,3 @@
-(function ($) {
-  'use strict';
-  new Sakura('.sakura-falling');
-})(jQuery);
-
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal.
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
 $(document).on('click', function () {
   document.getElementById('my_audio').play();
   console.log('Shaadi me zaroor aana');
@@ -69,6 +50,8 @@ console.log(
 
 document.addEventListener('DOMContentLoaded', () => {
   // Unix timestamp (in seconds) to count down to
+  new Sakura('.sakura-falling');
+  document.getElementById('my_audio').play();
   var twoDaysFromNow = new Date().getTime() / 1000 + 86400 * 2 + 1;
 
   // Set up FlipDown
